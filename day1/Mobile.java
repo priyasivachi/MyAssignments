4091c8c8f041946df989c99ee91a1fdcf4128705
priyasivachi <104180817+priyasivachi@users.noreply.github.com>
2023-02-04 19:37:58 -0800

    Add files via upload
    day1

---
+++ day1/Mobile.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Mobile {
+	public static void main(String[] args) {
+		
+		String mobileBrandName="IPHONE10";
+		char mobileLogo='i';
+		short noofMobilePiece=20;
+		int modelNumber=14;
+		long mobilemeiNumber=86636920390500020l;
+		float mobilePrice=65500.87f;
+		boolean isDamaged=false;
+		
+		System.out.println(mobileBrandName);
+		System.out.println(mobileLogo);
+		System.out.println(noofMobilePiece);
+		System.out.println(modelNumber);
+		System.out.println(mobilemeiNumber);
+		System.out.println(mobilePrice);
+		System.out.println(isDamaged);
+		
+		
+				
+	}
+
+}
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		
+		int noOfWheels=2;
+		short noOfMirrors=2;
+		long chassisNumber=567890l;
+		boolean isPunctured=false;
+		String bikeName="BMW";
+		double runningKM=100.5;
+		System.out.println(noOfWheels);
+		System.out.println(noOfMirrors);
+		System.out.println(chassisNumber);
+		System.out.println(isPunctured);
+		System.out.println(bikeName);
+		System.out.println(runningKM);
+
+		
+		
+		
+	}
+
+}
