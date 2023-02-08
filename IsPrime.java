cb2535119ee1690ac1f9d7ae9a4f0d8fafc0e1eb
Priyadharsini Jothivel <104180817+priyasivachi@users.noreply.github.com>
2023-02-08 10:40:26 -0800

    Update IsPrime.java

--- IsPrime.java
+++ IsPrime.java
@@ -4,20 +4,35 @@ package week1.day2;
 public class IsPrime {
 	
 	public static void main(String[] args) {
-		for(int i=2;i<=100;i++)
+		boolean flagg=false,flag=false;
+		int i=31;
+		
+		if(i%2==0) 
 		{
-			if(i%2==0)
-				System.out.println(i+ "the no is not prime");
-			else
-				System.out.println(i+"the no is prime");
+			flag=true;
+		}
+	if(i%2==1)
+			{
+		if(i%3==0) {
+				flagg=true;
+		
+			}
+			}
+		
+			
+		if(flagg==true || flag==true)
+			{
+				
+				System.out.println(i+ "the no is not  prime");}
+			else {
+				System.out.println(i+"the no is  prime");
 		}
 		
-		
-		
-		
-	}
-		}    
-		 
+	
+			}
+		}
+
+	
 
 
 
