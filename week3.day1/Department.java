f82bce547d1acd03fbb06255ed4b81a3a9e54d9f
Priyadharsini Jothivel <104180817+priyasivachi@users.noreply.github.com>
2023-02-19 12:31:37 -0800

    Add files via upload

---
+++ week3.day1/Department.java
@@ -0,0 +1,12 @@
+package org.department;
+
+import org.college.College;
+
+public class Department extends College {
+	public void deptName() {
+		System.out.println("The department name is MCA");
+		// TODO Auto-generated method stub
+
+	}
+
+}
