109799729fd97b6d31640527fa35f5ccd6996150
Priyadharsini Jothivel <104180817+priyasivachi@users.noreply.github.com>
2023-02-19 12:28:23 -0800

    Add files via upload

---
+++ week3.day1/College.java
@@ -0,0 +1,20 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("My college name is IRTT");
+		// TODO Auto-generated method stub
+
+	}
+
+	public void collegeCode() {
+		System.out.println("Code is 301");
+		// TODO Auto-generated method stub
+
+	}
+	public void collegeRank() {
+		System.out.println("college rank is 10");
+		// TODO Auto-generated method stub
+
+	}
+}
