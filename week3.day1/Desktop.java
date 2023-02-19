d6f0af51cae325c4fb396786c6eac378cceff688
Priyadharsini Jothivel <104180817+priyasivachi@users.noreply.github.com>
2023-02-19 12:30:35 -0800

    Add files via upload

---
+++ week3.day1/Desktop.java
@@ -0,0 +1,16 @@
+package org.system;
+
+public class Desktop extends Computer {
+	
+	
+
+
+	public static void main(String[] args) {
+		
+		Desktop desk=new Desktop();
+		desk.computerModel();
+		// TODO Auto-generated method stub
+
+	}
+
+}
