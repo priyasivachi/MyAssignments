1487c30200bb014060af7144c2b0237c0c13fbef
Priyadharsini Jothivel <104180817+priyasivachi@users.noreply.github.com>
2023-02-19 12:30:06 -0800

    Add files via upload

---
+++ week3.day1/Computer.java
@@ -0,0 +1,12 @@
+package org.system;
+
+public class Computer {
+	
+
+	public void computerModel() {
+		System.out.println("This is Computer from Parent");
+		// TODO Auto-generated method stub
+
+	}
+
+}
