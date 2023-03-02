8e371185609ee18265cb21dceb766b4fa78242e9
Priyadharsini Jothivel <104180817+priyasivachi@users.noreply.github.com>
2023-03-02 13:02:36 -0800

    Add files via upload

---
+++ Week1.day2/IsPrime.java
@@ -0,0 +1,93 @@
+package week1.day2;
+
+
+public class IsPrime {
+	
+	public static void main(String[] args) {
+		boolean flag=false;
+		int n=17,i,k;
+		
+		for(i=2;i<n;i++)
+		
+		{	
+		
+		if(n%i==0) 
+		{
+			flag=true;
+		
+			break;
+		}
+		}
+		
+		if(flag==true )
+			{
+				
+				System.out.println(n+ "the no is  not  prime");
+				}
+			else {
+				System.out.println(n+"the no is prime");
+		}
+		
+		for(k=2;k<100;k++)
+		{
+			boolean flagg=false;
+			
+			for(int j=2;j<k;j++) {
+			
+				if(k%j==0)
+				{
+					flagg=true;
+					break;
+				}
+				}
+		
+		if(flagg==false)
+		{
+			System.out.println(k+ "the no is  prime");
+		}
+	}
+		
+			
+		
+		
+		
+		
+}
+			}
+		
+
+		
+		
+	
+
+	
+
+
+
+		
+		
+	
+		    
+		 
+
+
+
+
+	
+	
+
+
+	
+	
+	
+
+
+	
+	
+	
+	
+
+
+	
+	
+
